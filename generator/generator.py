d44b710246505b6a5a7db0f96612b0f0f8c3b72f
Sema <123366660+Semafftp@users.noreply.github.com>
2023-07-18 23:14:41 +0700

    Add files via upload

--- generator/generator.py
+++ generator/generator.py
@@ -1,9 +1,11 @@
 import random
 
-from data.data import Person
+from data.data import Person, Color,Date
 
 from faker import Faker
+
 faker_ru = Faker('ru_RU')
+faker_en = Faker('EN')
 Faker.seed()
 
 
@@ -28,3 +30,20 @@ def generated_file():
     file.write(f'Hello World{random.randint(0, 999)}')
     file.close()
     return file.name, path
+
+
+def generated_color():
+    yield Color(
+        color_name=["Red", "Blue", "Green", "Yellow", "Purple", "Black", "White", "Voilet", "Indigo", "Magenta", "Aqua"]
+    )
+
+
+def generated_date():
+    yield Date(
+        year = faker_en.year(),
+        month = faker_en.month_name(),
+        day = faker_en.day_of_month(),
+        time = "12:00",
+    )
+
+
