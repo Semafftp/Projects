5b9faa295a31c7a5b5fdb1a579b5221302adbbe6
Sema <123366660+Semafftp@users.noreply.github.com>
2023-08-08 18:42:44 +0700

    Add files via upload

--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,4 +1,4 @@
-from pages.interactions_page import SortablePage
+from pages.interactions_page import SortablePage, SelectablePage, ResizablePage
 
 
 class TestInteractions:
@@ -12,3 +12,25 @@ class TestInteractions:
             grid_before, grid_after = sortable_page.change_grid_order()
             assert list_before != list_after
             assert grid_before != grid_after
+
+    class TestSelectablePage:
+
+        def test_selectable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            item_list = selectable_page.select_list_item()
+            item_grid = selectable_page.select_grid_item()
+            assert len(item_list) > 0, "no elements were selected "
+            assert len(item_grid) > 0, "no elements were selected "
+
+
+class TestResizablePage:
+
+    def test_resizable(self, driver):
+        resizable_page = ResizablePage(driver, 'https://demoqa.com/resizable')
+        resizable_page.open()
+        max_box, min_box = resizable_page.change_size_resizable_box()
+        max_resize, min_resize = resizable_page.change_size_resizable()
+        assert ('500px', '300px') == max_box, "maximum size not equal to '500px', '300px'"
+        assert ('150px', '150px') == min_box, "minimum size not equal to '150px', '150px'"
+        assert min_resize != max_resize, "resizable has not been changed"
