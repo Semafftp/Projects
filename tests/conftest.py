888d3c704181a1c21961ba4ab67c52ace9d7fea4
Sema <123366660+Semafftp@users.noreply.github.com>
2023-08-20 18:01:19 +0700

    Add files via upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from datetime import datetime
+
+import allure
 
 
 @pytest.fixture
@@ -9,4 +12,6 @@ def driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
+    attach = driver.get_screenshot_as_png()
+    allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
     driver.quit()
